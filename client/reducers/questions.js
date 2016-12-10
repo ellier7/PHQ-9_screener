2742abdf0d54ac9ec1c01dc8dbd9e08b8f464698
ellier7 <eroussopoulos@gmail.com>
2016-12-10 18:09:03 -0500

    Answers rendering to page; Implemented action for when answer is selected

--- client/reducers/questions.js
+++ client/reducers/questions.js
@@ -1,39 +1,20 @@
-// function posts(state = [], action) {
-//   console.log("STATE", state)
-//   switch(action.type) {
-//     case 'INCREMENT_LIKES' :
-//       console.log("Incrementing Likes!!");
-//       const i = action.index;
-//       return [
-//         ...state.slice(0,i), // before the one we are updating
-//         {...state[i], likes: state[i].likes + 1},
-//         ...state.slice(i + 1), // after the one we are updating
-//       ]
-//     default:
-//       return state;
-//   }
-// }
-
-// export default posts;
-
-
 function questionnaire(state = [], action) {
 	// console.log("ACTIONINDEX", action.index)
   switch(action.type) {
     case 'INCREMENT_SCORE':
+          // console.log("QUESTION", question.question)
+        // console.log("ACTIONQUESTIONINDEX", action.index)
+        // console.log("SCORE", action.score)
+
            return state.map((question, index) => {
-          console.log("QUESTION", question.question)
-        if (index === action.index) {
-        console.log("ACTIONQUESTIONINDEX", action.index)
-        console.log("SCORE", action.score)
-          return Object.assign({}, question, {
+        if (index !== action.index) {
+          return Object.assign({}, question)
+      } else {
+      	 return Object.assign({}, question, {
             score: action.score
-          });
-        } else {
-          console.log("WHEN WILL I HIT HERE", index)
-          return Object.assign({}, question);
-        }
-      })
+          })
+      }
+  }
     default:
       return state;
   }
@@ -41,20 +22,3 @@ function questionnaire(state = [], action) {
 
 export default questionnaire;
 
-
-// export const questionAnswers = (state, { type, answerId, questionId }) => {
-//     console.log("initial STATE", initialState)
-//   switch(type) {
-//     case types.SELECT_ANSWER:
-//       return Object.assign({}, state, { [questionId]: answerId })
-//     default:
-//       return state
-//   }
-// }
-
-// const questionnaire = (state = initialState, action) => {
-//       // console.log("QUESTIONAIRE ANSWERS", state.questionAnswers)
-//   return Object.assign({}, state, {
-//     questionAnswers: questionAnswers(state.questionAnswers, action)
-//   })
-// }
