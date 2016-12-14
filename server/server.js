aa50b99d9e77c2c5e8c8a493ee8220534ac3821e
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-14 11:01:21 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 8000;
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 8000
 
-var app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
-
-// webpack loads index.html, looks for script src
 app.get('/public/bundle.js', function(req, res){
-  res.sendFile(path.join(__dirname, '../client/public/bundle.js'));
-});
+  res.sendFile(path.join(__dirname, '../client/public/bundle.js'))
+})
 
 app.get('/styles/style.css', function(req, res){
-  res.sendFile(path.join(__dirname, '../client/styles/style.css'));
-});
+  res.sendFile(path.join(__dirname, '../client/styles/style.css'))
+})
 
 app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 app.listen(port, function(){
-	console.log('Listening on port ' + port);
-});
+	console.log('Listening on port ' + port)
+})
