3f7ff17e1762704cb384b284b879fc54acaaa028
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-14 11:03:00 -0500

    Update Appointment.js

--- client/components/Appointment.js
+++ client/components/Appointment.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from 'react'
 
 const Appointment = React.createClass({
   render() {
     return (
          <div className="schedule">
-         A representative will call you to schedule an appointment. <br/> Thank you for taking the survey! 
+         	A representative will call you to schedule an appointment. 
+         		<br/> Thank you for taking the survey! 
          </div>
-    )
-  }
-});
+         )
+	}
+})
 
-export default Appointment;
+export default Appointment
