a1bdf8e159d49cb9109be0734dc4292daf261aa1
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-14 11:45:19 -0500

    Update actionCreators.js

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,4 +1,4 @@
-// increment
+// action creator to keep track of the question number and answer value when a user selects an answer
 export function increment(question, questionIndex, answerValue) {
   return {
     type: 'GET_ANSWERS',
