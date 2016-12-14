2d9f23b0307a94ae3c9de770883f7c74a8915d8d
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-14 11:47:44 -0500

    Update Answers.js

--- client/components/Answers.js
+++ client/components/Answers.js
@@ -3,9 +3,11 @@ import { ButtonGroup, Button } from 'react-bootstrap'
 import Submit from './Submit'
 
 const Answers = React.createClass({
+  // change color of button background so user can see which answer they selected
   onItemClick: function (event) {
     event.currentTarget.style.backgroundColor = 'ivory'
   },
+  // call action creator once button is clicked
   incrementScore: function(questions, questionIndex, answerValue, event){
     this.props.increment(questions, questionIndex, answerValue)
     this.onItemClick(event)
