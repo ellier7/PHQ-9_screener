f4f0be58f09999f6296b34083c974103b82bc2a8
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-23 17:20:11 -0500

    Update Questionnaire.js

--- client/components/Questionnaire.js
+++ client/components/Questionnaire.js
@@ -1,40 +1,69 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Button } from 'react-bootstrap'
-import { browserHistory } from 'react-router'
+
 import Questions from './Questions'
+import Submit from './Submit'
 
+import { submitAnswers } from '../actions/actionCreators'
 
-const Questionnaire = React.createClass({
-    onSubmit: function(e) {
-    e.preventDefault()
-    browserHistory.push('/results')
-  },
-   // button is disabled unless all questions have been answered
-  resultLength: function(){
-    return Object.keys(this.props.results).length !== 9
-  },
-  render() {
-    return (
-        <div className="main">
-          <h1 className="title">PHQ-9 Depression Screener</h1>
-          <h3 className="warning">Please answer all questions before submitting<br/></h3>
+class Questionnaire extends Component {
 
-      <div className="description">Over the last two weeks, how often have you been bothered by any of the following problems?
-        {this.props.questionnaire.map((questions, i) => 
-        <Questions 
-        	{...this.props} 
-        	key={i} 
-            questionIndex={i} 
-        	questions={questions.question}
-          />
-          )}
-        <form onSubmit={this.onSubmit} className="MyForm">
-          <Button bsSize="large" type="submit" disabled={this.resultLength()}>Submit</Button>
-        </form>
-      </div>
-      </div>
-    )
+  resultLength () {
+    return Object.keys(this.props.results).length !== 11
   }
-})
 
-export default Questionnaire
+  render () {
+    if (this.props.submitAnswer) {
+      return <Submit />
+    } else {
+      return (
+        <div className='main'>
+          <h1 className='title'>PHQ-9 Depression Screener</h1>
+          <h3 className='warning'>Please answer all questions before submitting<br /></h3>
+
+          <div className='description'>Over the last two weeks, how often have you been bothered by any of the following problems?
+      {this.props.questionnaire.map((questions, i) =>
+        <Questions
+          key={i}
+          questionIndex={i}
+          questions={questions.question}
+          />
+          )
+          }
+            <form onSubmit={this.props.submitAnswers} className='MyForm'>
+              <Button bsSize='large' type='submit' disabled={this.resultLength()}>Submit</Button>
+            </form>
+          </div>
+        </div>
+      )
+    }
+  }
+}
+
+const { array, object, func, bool } = React.PropTypes
+
+Questionnaire.propTypes = {
+  questionnaire: array.isRequired,
+  results: object.isRequired,
+  submitAnswers: func.isRequired,
+  submitAnswer: bool
+}
+
+function mapStateToProps (state) {
+  return {
+    questionnaire: state.questionnaire,
+    results: state.results,
+    submitAnswer: state.results.submitAnswer
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({
+    submitAnswers: submitAnswers
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Questionnaire)
+
