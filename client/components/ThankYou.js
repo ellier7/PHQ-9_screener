50adca7ce692ef169aba4ce8a1f7ea214e56d144
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-23 17:21:51 -0500

    Update ThankYou.js

--- client/components/ThankYou.js
+++ client/components/ThankYou.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-const ThankYou = React.createClass({
-  render() {
-    return (
-    	<div className="thanks">
-    		Thank you for taking the survey!
-    	</div>
-    	)
-	}
-})
+const ThankYou = () => (
+  <div className='thanks'>
+  Thank you for taking the survey!
+  </div>
+  )
 
 export default ThankYou
