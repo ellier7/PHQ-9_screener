0e74815b3879ab6c24c22fde004617083af31632
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-23 17:25:53 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -3,25 +3,14 @@ import { render } from 'react-dom'
 
 // Import Components
 import App from './components/App'
-import Questionnaire from './components/Questionnaire'
-import AnswerChoices from './components/AnswerChoices'
-import Submit from './components/Submit'
-import Appointment from './components/Appointment'
 
 // import react router deps
-import { Router, Route, IndexRoute } from 'react-router'
 import { Provider } from 'react-redux'
-import store, { history } from './store'
+import store from './store'
 
 const router = (
   <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Questionnaire}></IndexRoute>
-         <Route path="/results" component={Submit} />
-          <Route path="/appointment" component={Appointment} />
-      </Route>
-    </Router>
+    <App />
   </Provider>
 )
 
