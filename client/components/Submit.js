4a305ca88447f77e9fb2a2a5675cee95d4f4bdf0
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-23 17:21:35 -0500

    Update Submit.js

--- client/components/Submit.js
+++ client/components/Submit.js
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import DepressionSeverity from './DepressionSeverity'
 
-const Submit = React.createClass({
-  render() {
-    const { results } = this.props
+class Submit extends Component {
+  render () {
+    const { depressionSeverity } = this.props
     // calculate total score by adding values in results object
-    const score = Object.values(results).reduce((x,y) => x+y)
-    
+    const score = Object.values(this.props.results).reduce((x, y) => {
+      if (typeof x === 'number' && typeof y === 'number') {
+        return x + y
+      }
+      return x
+    })
     return (
-      <div className="score"><strong>Your scored a {score} out of 27</strong>
-      {this.props.depressionSeverity.map((range, i) =>
-        <DepressionSeverity 
-        {...this.props}
-        key={i}
-        text={range.text}
-        score={score}
-        low={range.low}
-        high={range.high}
+      <div className='score'><strong>Your scored a {score} out of 27</strong>
+        {depressionSeverity.map((range, i) => {
+          return <DepressionSeverity
+            key={i}
+            text={range.text}
+            score={score}
+            low={range.low}
+            high={range.high}
         />
-      )
+        })
     }
-        </div>
+      </div>
     )
   }
-})
+}
 
-export default Submit
+const { object, array } = React.PropTypes
+
+Submit.propTypes = {
+  results: object,
+  depressionSeverity: array
+}
+
+function mapStateToProps (state) {
+  return {
+    results: state.results,
+    depressionSeverity: state.depressionSeverity
+  }
+}
+
+export default connect(mapStateToProps)(Submit)
