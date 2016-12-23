c973a57156822696b22c0c260edbb2a972408728
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-23 17:24:49 -0500

    Update results.js

--- client/reducers/results.js
+++ client/reducers/results.js
@@ -1,11 +1,27 @@
-function results(state = [], action) {
-  const { question, questionIndex, answerValue } = action
+const initialState = {
+  submitAnswer: false,
+  submitTherapist: false
+}
+
+const results = (state = initialState, action) => {
+  const { questionIndex, answerValue } = action
   switch (action.type) {
-  case 'GET_ANSWERS':
-   return Object.assign({}, state, { [questionIndex]: answerValue })
+    case 'GET_ANSWERS':
+      return Object.assign({}, state, {
+        [questionIndex]: answerValue
+      })
+    case 'SUBMIT_ANSWERS':
+      return Object.assign({}, state, {
+        submitAnswer: true
+      })
+    case 'SUBMIT_THERAPISTS':
+      return Object.assign({}, state, {
+        submitTherapist: true
+      })
     default:
       return state
   }
 }
 
 export default results
+
