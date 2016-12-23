915cd9f60f09670f3fd0ade7d4441f0446300c02
Ellie Roussopoulos <ellier7@users.noreply.github.com>
2016-12-23 17:18:48 -0500

    Update Answers.js

--- client/components/Answers.js
+++ client/components/Answers.js
@@ -1,26 +1,46 @@
-import React from 'react'
-import { ButtonGroup, Button } from 'react-bootstrap'
-import Submit from './Submit'
+import React, {Component} from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Button } from 'react-bootstrap'
 
-const Answers = React.createClass({
-  // change color of button background so user can see which answer they selected
-  onItemClick: function (event) {
-    event.currentTarget.style.backgroundColor = 'ivory'
-  },
-  // call action creator once button is clicked
-  incrementScore: function(questions, questionIndex, answerValue, event){
-    this.props.increment(questions, questionIndex, answerValue)
-    this.onItemClick(event)
-  },
-  render() {
-    const { answer, questions, questionIndex, answerValue } = this.props
+import { increment } from '../actions/actionCreators'
+
+class Answers extends Component {
+  incrementScore (questionIndex, answerValue) {
+    this.props.increment(questionIndex, answerValue)
+  }
+
+  render () {
+    const { answer, questionIndex, answerValue } = this.props
     return (
-       <div className="buttons">
-        <Button className="wellStyles" bsSize="large" block
-        onClick={this.incrementScore.bind(this, questions, questionIndex, answerValue)}>{answer}</Button>
+      <div className='buttons'>
+        <Button className='wellStyles' bsSize='large' block
+          onClick={this.incrementScore.bind(this, questionIndex, answerValue)}>
+          {answer}
+        </Button>
       </div>
     )
   }
-})
+}
 
-export default Answers
+const { number, func } = React.PropTypes
+
+Answers.propTypes = {
+  answerValue: number.isRequired,
+  increment: func.isRequired
+}
+
+function mapStateToProps (state) {
+  return {
+    answers: state.answers
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({
+    increment: increment
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Answers)
+
